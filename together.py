eabf378125424a96675cce79680e604e1555bd6f
Nikk <nicholas.forbus@gmail.com>
2018-05-24 14:05:20 -0700

    Modified together.py

--- together.py
+++ together.py
@@ -4,22 +4,50 @@
 #   Alert user if there is NO potential danger
 # Step 3: Continue checking for recent kills (and make comparisons)
 
+import threading
 from network_test import build_graph_wrapper
 from zkillboard_info import get_Zkillboard_data
 
-char_location = '30001005'
-num_jumps = 2
 
 # Creates the list of adjacent systems, including base system
+def gather_systems(location, jumps):
+    systems = list(build_graph_wrapper(location, jumps))
+    systems = sorted(systems)
+    return systems
 
-systems = list(build_graph_wrapper(char_location, num_jumps))
 
 # Searches all systems to see if there was hostile activity recently
-#systems = list(map(int, systems))
-systems = sorted(systems)
-#systems = systems.sort(key=int)
-dangerSystems = get_Zkillboard_data(systems, 4)
+def check_for_danger(systems_to_check, time_range):
+    systems = get_Zkillboard_data(systems_to_check, time_range)
 
-print(dangerSystems)
+    if systems:
+        report_danger(systems)
 
 
+#if hostile activity, inform the user (No alert because this is a history search)
+def report_danger(systems_to_report):
+    print("The following systems are dangerous: \n")
+    print(systems_to_report)
+
+
+#Go into "refresh mode", where every X seconds, zkillboard is queried again.
+def refresh_scan(systems_to_check, rate, time_range):
+    threading.Timer(rate, refresh_scan).start()
+    check_for_danger(systems_to_check, time_range)
+
+
+#default parameters
+char_location = '30001005'
+num_jumps = 2
+refresh_rate = 15.0
+initial_time = 4
+recur_time = .016
+
+#initial check
+adj_systems = gather_systems(char_location, num_jumps)
+check_for_danger(adj_systems, initial_time)
+
+print("Onto recurrence: \n")
+
+#recurring checks
+refresh_scan(adj_systems, refresh_rate, recur_time)
